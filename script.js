59a23c83a4904cae48fc600772738407eb035e19
Ming Di Leom <43627182+curbengh@users.noreply.github.com>
2025-04-01 07:53:09 +0000

    refactor: fetch(), ESM, top-level await

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const { request } = require('@octokit/request')
-const { stream: gotStream } = require('got')
-const { createWriteStream } = require('fs')
-const { pipeline } = require('stream/promises')
-const { ParseOne: unzipOne } = require('unzipper')
-const { writeFile } = require('fs/promises')
+import { Readable } from 'node:stream'
+import { request } from '@octokit/request'
+import { createWriteStream } from 'node:fs'
+import { pipeline } from 'stream/promises'
+import { ParseOne as unzipOne } from 'unzipper'
+import { writeFile } from 'node:fs/promises'
 const { env } = process
 
 const requestWithAuth = request.defaults({
@@ -14,39 +14,35 @@ const requestWithAuth = request.defaults({
   repo: 'FreeTube'
 })
 
-const f = async () => {
-  let artifact_id = ''
-  let name = 'freetube-0.23.2-nightly-5718-amd64.pacman'
-  let headSha = ''
-  let build = ''
-  let tag = ''
-  let releaseTag = ''
-  let workflowId = ''
-  const res = await requestWithAuth('GET /repos/{owner}/{repo}/actions/artifacts')
+let artifactId = ''
+let name = 'freetube-0.23.2-nightly-5718-amd64.pacman'
+let headSha = ''
+let build = ''
+let tag = ''
+let releaseTag = ''
+let workflowId = ''
+const res = await requestWithAuth('GET /repos/{owner}/{repo}/actions/artifacts')
 
-  for (const artifact of res.data.artifacts) {
-    if (artifact.name.endsWith('.pacman')) {
-      artifact_id = artifact.id
-      name = artifact.name
-      headSha = artifact.workflow_run.head_sha
-      workflowId = artifact.workflow_run.id
-      // 5718
-      build = name.split('-')[3]
-      // 0.23.2
-      tag = name.split('-')[1]
-      releaseTag = `${tag}.build${build}.${headSha.substring(0, 7)}`
-      await writeFile('setenv.txt', `release_tag=${releaseTag}\nworkflow_id=${workflowId}\n`)
-      break
-    }
+for (const artifact of res.data.artifacts) {
+  if (artifact.name.endsWith('.pacman')) {
+    artifactId = artifact.id
+    name = artifact.name
+    headSha = artifact.workflow_run.head_sha
+    workflowId = artifact.workflow_run.id
+    // 5718
+    build = name.split('-')[3]
+    // 0.23.2
+    tag = name.split('-')[1]
+    releaseTag = `${tag}.build${build}.${headSha.substring(0, 7)}`
+    await writeFile('setenv.txt', `release_tag=${releaseTag}\nworkflow_id=${workflowId}\n`)
+    break
   }
-
-  const dl = await requestWithAuth('GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/zip', { artifact_id })
-
-  await pipeline(
-    gotStream(dl.url),
-    unzipOne(),
-    createWriteStream('freetube.pacman.tar.xz')
-  )
 }
 
-f()
+const dl = await requestWithAuth('GET /repos/{owner}/{repo}/actions/artifacts/{artifactId}/zip', { artifactId })
+
+await pipeline(
+  Readable.fromWeb((await fetch(dl.url)).body),
+  unzipOne(),
+  createWriteStream('freetube.pacman.tar.xz')
+)
